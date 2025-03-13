b6e2a4f2e70cbb72d194f1f31fee320d2f868d88
RCHD88 <richardraferguy@gmail.com>
2025-03-14 01:26:27 +0700

    m2 home done

--- M2/M2_PRAK_PROJECT/app/src/main/java/com/example/m2_prak_project/MainActivity.kt
+++ M2/M2_PRAK_PROJECT/app/src/main/java/com/example/m2_prak_project/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.m2_prak_project
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.UnderlineSpan
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +21,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val tvTitle = findViewById<TextView>(R.id.tvTitle)
+
+        // Membuat teks "H A N G M A N" dengan garis bawah di setiap huruf
+        val word = "H A N G M A N"
+        val spannable = SpannableString(word)
+        for (i in word.indices step 2) { // Step 2 agar hanya huruf yang diberi garis bawah
+            spannable.setSpan(
+                UnderlineSpan(),
+                i,
+                i + 1,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+
+        tvTitle.text = spannable
+        tvTitle.typeface = Typeface.DEFAULT_BOLD
     }
 }

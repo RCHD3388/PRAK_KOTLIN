e2cf7ce274af8f8017220a579a15b4daeadb3830
RCHD88 <richardraferguy@gmail.com>
2025-03-20 02:50:48 +0700

    -add

--- M3/M3_PROJECT/app/src/main/java/com/example/m3_project_praktikum/AddMovieActivity.kt
+++ M3/M3_PROJECT/app/src/main/java/com/example/m3_project_praktikum/AddMovieActivity.kt
@@ -5,16 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.m3_project_praktikum.databinding.ActivityAddMovieBinding
 
 class AddMovieActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddMovieBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_add_movie)
+
+        binding = ActivityAddMovieBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.btnBack3.setOnClickListener { finish() }
     }
 }

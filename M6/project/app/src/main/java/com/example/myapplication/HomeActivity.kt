23ab749e64f235ef4267e4e759bcb38d4fb054cf
RCHD88 <richardraferguy@gmail.com>
2025-04-26 04:13:39 +0700

    checkpoint profile

--- M6/project/app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ M6/project/app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
 import com.example.myapplication.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -15,5 +16,18 @@ class HomeActivity : AppCompatActivity() {
         enableEdgeToEdge()
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home);
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            if(it.itemId == R.id.im_home){
+                navController.navigate(R.id.action_global_homeFragment)
+            } else if(it.itemId == R.id.im_profile){
+                navController.navigate(R.id.action_global_profileFragment)
+            }
+            true
+        }
     }
 }
